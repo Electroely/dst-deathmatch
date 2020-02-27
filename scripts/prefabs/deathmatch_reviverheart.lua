14b6c38e81392660b4760d7c2e4036ba77776808
Electroely <elyas.maggouh@gmail.com>
2020-02-27 13:43:34 -0500

    Heart now gets dropped when owner is hit

--- scripts/prefabs/deathmatch_reviverheart.lua
+++ scripts/prefabs/deathmatch_reviverheart.lua
@@ -27,14 +27,52 @@ local function startbeat(inst)
     inst.beattask = inst:DoTaskInTime(.75 + math.random() * .75, beat)
 end
 
+local function launchitem(item, angle)
+    local speed = 3+math.random()*1
+    angle = (angle+math.random()*30-15) * DEGREES
+    item.Physics:SetVel(speed * math.cos(angle), 10, -speed * math.sin(angle))
+end
+
+local function onholderattacked(player, data)
+	if not player._droppedreviverheart then --one heart per attack
+		local heart = player.components.inventory:FindItem(function(item)
+			return item.prefab == "deathmatch_reviverheart"
+		end)
+		if heart ~= nil then
+			player.components.inventory:DropItem(heart)
+			heart.Transform:SetPosition(player.Transform:GetWorldPosition())
+			launchitem(heart, 
+				data.attacker and player:GetAngleToPoint(data.attacker.Transform:GetWorldPosition()) 
+				or math.random()*360)
+			player._droppedreviverheart = true
+		end
+		player:DoTaskInTime(0, function(player) 
+			player._droppedreviverheart = nil
+		end)
+	end
+end
+
 local function ondropped(inst)
+	if inst._listening then
+		inst:RemoveEventCallback("attacked", onholderattacked, inst._owner)
+		inst._owner = nil
+	end
     if inst.beattask ~= nil then
         inst.beattask:Cancel()
     end
     inst.beattask = inst:DoTaskInTime(0, startbeat)
 end
 
-local function onpickup(inst) --TODO player should drop heart if takes enough damage
+local function onpickup(inst, owner)
+	if inst._listening then
+		inst:RemoveEventCallback("attacked", onholderattacked, inst._owner)
+		inst._owner = nil
+	end
+	if owner ~= nil then
+		inst:ListenForEvent("attacked", onholderattacked, owner)
+		inst._owner = owner
+		inst._listening = true
+	end
     if inst.beattask ~= nil then
         inst.beattask:Cancel()
         inst.beattask = nil
@@ -52,7 +90,9 @@ local function fn()
     inst.entity:AddAnimState()
     inst.entity:AddSoundEmitter()
     inst.entity:AddNetwork()
-
+	
+	inst:AddTag("irreplaceable")
+	
     MakeInventoryPhysics(inst)
 
     inst.AnimState:SetBank("bloodpump")
