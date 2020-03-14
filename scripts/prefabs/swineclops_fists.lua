e5e28602e5611d78a115acf839b8ebd0743b3249
Hornet <alsaadiomar74@gmail.com>
2020-03-13 21:35:29 -0400

    972bq42v8awekisgdx

--- scripts/prefabs/swineclops_fists.lua
+++ scripts/prefabs/swineclops_fists.lua
@@ -3,11 +3,31 @@ local assets = {}
 local prefabs = {}
 
 local function OnEquip(inst, owner)
-	owner.AnimState:OverrideSymbol("hand", "lavaarena_beetlesaur", "hand")
+	owner.AnimState:OverrideSymbol("hand", "lavaarena_beetletaur", "hand")
 end
 
 local function OnUnEquip(inst, owner)
 	owner.AnimState:ClearOverrideSymbol("hand")
+end
+
+local function ReticuleTargetFn()
+    local player = ThePlayer
+    local ground = TheWorld.Map
+    local pos = Vector3()
+    for r = 7, 0, -.25 do
+        pos.x, pos.y, pos.z = player.entity:LocalToWorldSpace(r, 0, 0)
+        if ground:IsPassableAtPoint(pos:Get()) and not ground:IsGroundTargetBlocked(pos) then
+            return pos
+        end
+    end
+    return pos
+end
+
+local function AnvilStrike(inst, caster, pos)
+	caster:PushEvent("combat_leap", {
+		targetpos = pos,
+		weapon = inst
+	})
 end
 
 local function fn()
@@ -18,6 +38,19 @@ local function fn()
 	inst.entity:AddNetwork()
 		
 	MakeInventoryPhysics(inst)
+	
+	inst:AddTag("weapon")
+    inst:AddTag("aoeweapon_leap")
+    inst:AddTag("rechargeable")
+	
+	inst:AddComponent("aoetargeting")
+    inst.components.aoetargeting.reticule.reticuleprefab = "reticuleaoe"
+    inst.components.aoetargeting.reticule.pingprefab = "reticuleaoeping"
+    inst.components.aoetargeting.reticule.targetfn = ReticuleTargetFn
+    inst.components.aoetargeting.reticule.validcolour = { 1, .75, 0, 1 }
+    inst.components.aoetargeting.reticule.invalidcolour = { .5, 0, 0, 1 }
+    inst.components.aoetargeting.reticule.ease = true
+    inst.components.aoetargeting.reticule.mouseenabled = true
 
 	inst.entity:SetPristine()
 
@@ -29,6 +62,16 @@ local function fn()
 		
 	inst:AddComponent("inventoryitem")
 	
+	inst:AddComponent("aoespell")
+	inst.components.aoespell:SetAOESpell(AnvilStrike)
+	
+	inst:AddComponent("rechargeable")
+	
+	inst:AddComponent("aoeweapon_leap")
+	inst.components.aoeweapon_leap:SetDamage(100)
+	inst.components.aoeweapon_leap:SetRange(4.1)
+	inst.components.aoeweapon_leap:SetStimuli("electric")
+	
 	inst:AddComponent("weapon")
 	inst.components.weapon:SetDamage(100)
 	inst.components.weapon:SetOverrideStimuliFn(function(inst)
@@ -37,8 +80,8 @@ local function fn()
 
 	inst:AddComponent("equippable")
 	inst.components.equippable.equipslot = EQUIPSLOTS.HANDS
-	inst.components.equippable:SetOnEquip(OnUnEquip)
-	inst.components.equippable:SetOnUnequip(OnEquip)
+	inst.components.equippable:SetOnEquip(OnEquip)
+	inst.components.equippable:SetOnUnequip(OnUnEquip)
 
 	return inst
 end
