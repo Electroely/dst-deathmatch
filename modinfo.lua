323eef28bfdf58d70016029289acd789b1233127
Electroely <30663570+Electroely@users.noreply.github.com>
2021-01-13 13:38:28 -0500

    Workshop Release 20.1

--- modinfo.lua
+++ modinfo.lua
@@ -10,7 +10,7 @@ description = "WIP"
 author = "Electroely"
 
 --A version number so you can ask people if they are running an old version of your mod.
-version = "20.01"
+version = "20.1"
 
 --This lets other players know if your mod is out of date. This typically needs to be updated every time there's a new game update.
 api_version = 10
